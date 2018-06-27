134ee62d570c8dbb9b3c917b583dd716ec2f502e
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-06-27 13:23:07 +0900

    make movie_app with React 영화목록을 fetch를 이용해 불러오기

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -4,37 +4,10 @@ import Movie from './Movie';
 
 class App extends Component {
 
-  state = {
-    
-  }
+  state = {}
 
   componentDidMount(){
-    setTimeout(()=> {
-      this.setState({
-        movies: [
-          {
-            title : "star war",
-            poster : "http://t1.daumcdn.net/movie/526855c211c94b5b202bcc1edd75220aa151a7a3"
-          },
-          {
-            title : "지웅",
-            poster : "https://s-i.huffpost.com/gen/3839686/thumbs/o-POTATO-570.jpg?4"
-          },
-          {
-            title : "윤재",
-            poster : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1dvDdz8cfojgr3Zc_9U5skitra9NfTZocQ9IQhj1x6BrSEUhhwA"
-          },
-          {
-            title : "기강",
-            poster : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWdQAOgXzI65lehGERs6Wp1jGqaTyHXQVYEZDgFLObiA-VYFui"
-          },
-          {
-            title : "규준",
-            poster : "http://pds.joins.com/news/component/htmlphoto_mmdata/201705/23/ec7a8507-b3f8-404d-a2b9-a2ecababd400.jpg"
-          },
-        ]
-      })
-    }, 3000)
+    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating');
   }
 
   _renderMovie = () => {
