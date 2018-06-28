7cc5d62ebf451b0820d3e5678470a2a448f9cc46
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-06-28 17:02:06 +0900

    make movie_app with React 리액트로 영화앱 만들기. 완성

--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -37,6 +37,7 @@
  
 import React from 'react';
 import PropTypes from 'prop-types';
+import LinesEllipsis from 'react-lines-ellipsis';
 import './Movie.css';
 
 function Movie({title, poster, genres, synopsis}) {
@@ -50,9 +51,15 @@ function Movie({title, poster, genres, synopsis}) {
                 <div className = "Movie__Genres">
                     {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
                 </div>
-                <p className="Movie__Synopsis">
-                    {synopsis}
-                </p>
+                <div className="Movie__Synopsis">
+                <LinesEllipsis
+                    text={synopsis}
+                    maxLine='3'
+                    ellipsis='...'
+                    trimRight
+                    basedOn='letters'
+                    />   
+                </div>
             </div>
         </div>
     )
