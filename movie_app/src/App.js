df954b484bfbd478e5d8d651abfb36647b1f8eec
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-06-28 10:35:33 +0900

    make movie_app with React 영화 목록 업데이트

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -7,14 +7,28 @@ class App extends Component {
   state = {}
 
   componentDidMount(){
-    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating');
+    this._getMovies();
   }
 
   _renderMovie = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key ={index} />
+    const movies = this.state.movies.map(movie => {
+      return <Movie title={movie.title} poster={movie.large_cover_image} key ={movie.id} />
     })
-    return movies;
+    return movies
+  }
+
+  _getMovies= async () =>{
+     const movies = await this._callApi()
+     this.setState({
+       movies
+     })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(potato => potato.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
   }
 
   render() {
