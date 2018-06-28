d36aa18d0a27f14692bd29e5458908f910e838e0
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-06-28 13:11:38 +0900

    make movie_app with React db파일 불러오기

--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -35,38 +35,55 @@
     }
 */
  
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component {
-
-    static propTypes = {
-        title : PropTypes.string.isRequired,
-        poster : PropTypes.string.isRequired
-    }
-
-    render(){
-        return(
-            <div>
-                <MoviePoster poster={this.props.poster}/>
-                <h1>{this.props.title}</h1>
+function Movie({title, poster, genres, synopsis}) {
+    return (
+        <div className="Movie">
+            <div className="Movie__Columns">
+                <MoviePoster poster={poster} alt={title}/>
             </div>
-        )
-    }
+            <div className="Movie__Columns">
+                <h1>{title}</h1>
+                <div className = "Movie__Genres">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+                </div>
+                <p className="Movie__Synopsis">
+                    {synopsis}
+                </p>
+            </div>
+        </div>
+    )
 }
 
-class MoviePoster extends Component {
-
-static propTypes = {
-    poster : PropTypes.string.isRequired
+function MoviePoster({poster, alt}){
+    return (
+        <img src ={poster} alt={alt} title={alt} className="Movie__Poster"/>
+    )
 }
 
-    render(){
-        return(
-            <img src={this.props.poster} />
-        )
-    }
+function MovieGenre({genre}) {
+    return(
+        <span className = "Movie__Genre">{genre}</span>
+    )       
 }
 
-export default Movie;
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
+}
+
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
+}
+
+MovieGenre.propTypes = {
+    genre: PropTypes.string.isRequired
+}
+
+export default Movie
