65c9e0b161642f374ec423bdb358e93ec759c50e
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-07-09 23:48:18 +0900

    make Todolist-Form function

--- todo-list2/src/components/Form.js
+++ todo-list2/src/components/Form.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Form.css';
 
-const Form = ({value, onChange, onCreate, onKeypress}) => {
+const Form = ({value, onChange, onCreate, onKeyPress}) => {
     return(
         <div className="form">
-            <input value={value} onChange={onChange} onKeyPress={onKeypress}/>
-            <button className="create-button" onCreate={onCreate}>
+            <input value={value} onChange={onChange} onKeyPress={onKeyPress}/>
+            <button className="create-button" onClick={onCreate}>
                 Add
             </button>
         </div>
-    );
+    );  // onClick = {onCreate} 에서 {}안에 있는 값이 매개변수 값. 앞에꺼는 내장 함수
 };
 
 export default Form;
