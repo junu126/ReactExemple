133ed4f0ea3c21c2381413121a8fa1138daf8c5e
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-07-11 00:00:07 +0900

    make todolist - 오류수정 onRemove함수만 수정하면됨.

--- todo-list2/src/components/TodoItme.js
+++ todo-list2/src/components/TodoItme.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Todoitem.css';
 
-function TodoItem({text, check, onRemove, onToggle, id }) {
+const TodoItem = ({text, check, onRemove, onToggle, id }) => {
     return( // &times = X자. 곱하기 모양.
         <div className="todolist" onClick={() => onToggle(id)}>
             <div className="remove" onClick={(e) => {
-                e.stopPropagation();
+                e.stopPropagation()
                 onRemove(id)
             }}>&times;</div>
             <div className={`todolist ${check ? 'true' : 'false'}`}>
