f20bf9bab119f424b743d08b070f2783a300cc4f
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-07-11 17:12:48 +0900

    Make TodoList - finish the code

--- todo-list2/src/components/TodoItme.js
+++ todo-list2/src/components/TodoItme.js
@@ -3,12 +3,12 @@ import './Todoitem.css';
 
 const TodoItem = ({text, check, onRemove, onToggle, id }) => {
     return( // &times = X자. 곱하기 모양.
-        <div className="todolist" onClick={() => onToggle(id)}>
+        <div className="todolist">
             <div className="remove" onClick={(e) => {
-                e.stopPropagation()
                 onRemove(id)
+                console.log(onRemove(id) + '2');
             }}>&times;</div>
-            <div className={`todolist ${check ? 'true' : 'false'}`}>
+            <div onClick={() => onToggle(id)} className={`todolist ${check ? 'true' : 'false'}`}>
                 <div>{text}</div>
             </div>
         </div>
