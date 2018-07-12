3c89f69e4d66c3579fb39f48d2c1f09c86bb338f
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-07-12 23:38:32 +0900

    make weatherlist - list.js

---
+++ weather-list/src/component/weatherlist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './weatherlist.css';
+
+const weatherlist = ({}) => {
+    return(
+        <div>
+            
+        </div>
+    );
+}
+
+export default weatherlist;
