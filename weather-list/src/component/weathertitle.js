1e18f7dcb6b1b93ed8324b3afbd30e1934a7dea2
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-07-12 23:39:09 +0900

    make weatherlist - title.js

---
+++ weather-list/src/component/weathertitle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './weathertitle.css'
+
+const weathertitle = ({}) => {
+    return(
+        <div>
+            
+        </div>
+    );
+}
+
+export default weathertitle;
