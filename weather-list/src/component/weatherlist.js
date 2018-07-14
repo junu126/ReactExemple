e1c66776ba0c61e7cfa0031167abef2e12c442ea
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-07-14 23:55:50 +0900

    make WeatherList - 데이터 받아오기

--- weather-list/src/component/weatherlist.js
+++ weather-list/src/component/weatherlist.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './weatherlist.css';
 
-const weatherlist = ({}) => {
+const Weatherlist = ({}) => {
     return(
         <div>
             
@@ -9,4 +9,4 @@ const weatherlist = ({}) => {
     );
 }
 
-export default weatherlist;
+export default Weatherlist;
