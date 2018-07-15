c1facfdfbd1611f5a1c90c2c612f0c5b6476a0b0
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-07-15 22:48:42 +0900

    make WeatherList - 비 구현

--- weather-list/src/component/weatherlist.js
+++ weather-list/src/component/weatherlist.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './weatherlist.css';
 
-const Weatherlist = ({}) => {
+const Weatherlist = ({rain_img, rain}) => {
     return(
         <div>
-            
+            <div className="list-humidity_temperature">
+                
+            </div>
+            <div className="list-sky">
+                
+            </div>
+            <div className="list-wind_rain">
+                <img src={rain_img}></img>
+                <span>{rain}</span>
+            </div>
         </div>
     );
 }
