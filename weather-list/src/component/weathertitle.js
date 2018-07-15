056b172c699b2e70e4b29b05e331055373a8646d
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-07-15 22:48:52 +0900

    make WeatherList - 비 구현

--- weather-list/src/component/weathertitle.js
+++ weather-list/src/component/weathertitle.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './weathertitle.css'
-import { isMoment } from '../../node_modules/moment';
 
-const Weathertitle = ({bd}) => {
+const Weathertitle = ({bd, }) => {
     return(
         <div className="title">
+        
             <span className="date">{bd}</span>
             <span className="text">WEATHER-LIST</span>
             <footer className="mini-text">in Daejeon</footer>
