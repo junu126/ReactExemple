eec9b6ff28281e5017f0d1b61891f6b019582159
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-08-20 23:57:38 +0900

    Fullpage library exemple

--- notice/src/App.js
+++ notice/src/App.js
@@ -1,19 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./Component/CSS/fullpage.css";
+import "./Component/CSS/examples.css";
+import "./Component/fullpage.js";
+import "./Component/examples.js";
 
 class App extends Component {
+
+  script = () => {
+    var myFullpage = new fullpage('#fullpage', {
+      sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE'],
+      css3: false
+    });
+  }
+
+  componentDidMount() {
+    const scriptA = document.createElement("script");
+    const scriptB = document.createElement("script");
+    const scriptC = document.createElement("script");
+
+    scriptA.src = "./Component/fullpage.js"
+    scriptA.type = 'text/javascript';
+    scriptB.src = "./Component/examples.js";
+    scriptB.type = 'text/javascript';
+    scriptC.appendChild(() => {this.script})
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div id="fullpage" class="fullpage-wrapper">
+          <div class="section fp-section fp-table active fp-completely" id="section0" data-fp-styles="null">
+            <h1>fullPage.js</h1>
+            <p>CSS3 is active by default</p>
+          </div>
+          <div class="section fp-section fp-table" id="section1" data-fp-styles="null"><div class="fp-tableCell">
+            <div class="intro">
+              <h1>Speed on mobile</h1>
+              <p>
+                If CSS3 transforms are supported (e.g. on mobile devices) they will be used for animations, usually a good
+                choice if the animation on mobile devices is slow.
+              </p>
+            </div>
+          </div></div>
+          <div class="section fp-section fp-table" id="section2" data-fp-styles="null"><div class="fp-tableCell">
+            <div class="intro">
+              <h1>No CSS3? No problem!</h1>
+              <p>If CSS3 is not available, animations will fall back to javascript driven animations.</p>
+            </div>
+          </div></div>
+        </div>
     );
   }
 }
