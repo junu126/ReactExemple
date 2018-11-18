a1098af0d4fcdd8fcbbd755e01eb2f15e539115c
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-11-18 09:07:20 +0900

    Types

---
+++ todolist_entry-redux/src/core/redux/action/types.js
@@ -0,0 +1,8 @@
+// 리스트 추가
+export const ADD_TODOLIST = 'ADD_TODOLIST';
+
+// 리스트 제거
+export const DELETE_TODOLIST = 'DELETE_TODOLIST';
+
+// 리스트 체크
+export const ON_HANDLE_TOGGLE = 'ON_HANDLE_TOGGLE';
