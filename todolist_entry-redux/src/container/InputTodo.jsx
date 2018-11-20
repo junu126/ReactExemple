792fb5319da08a3f03435a29f47c6f959d49e37a
DESKTOP-DT7IFC9\10406KJW <junu321@naver.com>
2018-11-20 22:56:55 +0900

    ADD : 투두 입력 하는 컨테이너

---
+++ todolist_entry-redux/src/container/InputTodo.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { addTodoList, isHandleInput } from '../core/redux/action/action'
+import { InputBar } from '../components/index';
+import { connect } from 'react-redux';
+
+class InputTodo extends Component {
+
+  addTodoList = (e) => {
+    this.props.addTodoList(e.target.value);
+    e.target.value = null;
+  }
+
+  isHandleInput = (e) => {
+    this.props.isHandleInput(e.target.value);
+  }
+
+  render() { 
+    return ( 
+      <InputBar 
+        addTodoList = {this.addTodoList}
+        isHandleInput = {this.isHandleInput}
+      />
+     );
+  }
+}
+
+let mapDispatchToProps = (dispatch) => ({
+  addTodoList : text => dispatch(addTodoList(text)),
+  isHandleInput : value => dispatch(isHandleInput(value))
+});
+ 
+export default connect(
+  null,
+  mapDispatchToProps
+)(InputTodo);
