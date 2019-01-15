ab0801c727c7f7d5816667c544b8464184b0b4f7
Kim Jun Woo <junukim.dev@gmail.com>
2019-01-15 22:50:52 +0900

    ADD : Root bundlling file

---
+++ react_animaion/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import AnimatedShoppingList from './source/AnimatedShoppingList'
+
+class App extends Component {
+  render() {
+    return (
+      <AnimatedShoppingList />
+    );
+  }
+}
+
+export default App;
