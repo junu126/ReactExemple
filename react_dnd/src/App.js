227a1c4bdeb815bd8782e5f4c96ab07fcd1c2225
Kim Jun Woo <junukim.dev@gmail.com>
2019-01-17 15:18:59 +0900

    ADD : Render FIle Contents Item

---
+++ react_dnd/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Container from './source/Container';
+
+class App extends Component {
+  render() {
+    return (
+      <Container />
+    );
+  }
+}
+
+export default App;
