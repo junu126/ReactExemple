c71bcd8e61c0d0b2d161250dd5a9acb95dba9278
Kim Jun Woo <junukim.dev@gmail.com>
2019-02-27 14:47:39 +0900

    CREATE : make star with css

---
+++ react_css_clip-path/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+  <div>
+    <Star />
+    <Star />
+    <Star />
+    <Star />
+    <Star />
+  </div>
+);
+
+const Star = () => (
+  <div className='star'></div>
+)
+
+export default App;
